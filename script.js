22ddae1acdec2cd16e6a8e5909d51425b4b7445b
Moritz Hofmann <mowmoh@gmail.com>
2022-02-14 11:20:49 +0100

    Clean the code

--- script.js
+++ script.js
@@ -1,23 +1,17 @@
 // Create a function called computerPlay
 
 function computerPlay() {
-
-// Let the function computerPlay return either 'Rock', 'Paper', or 'Scissors'
-// We’ll use this function in the game to make the computer’s play
-
-let choice = ['Rock', 'Paper', 'Scissors'];
-let random = Math.floor((Math.random()*3));
-
-return choice[random];
-
-
+    // Let the function computerPlay return either 'Rock', 'Paper', or 'Scissors'
+    // We’ll use this function in the game to make the computer’s play
+    let choice = ['Rock', 'Paper', 'Scissors'];
+    let random = Math.floor((Math.random()*3));
+    return choice[random];
 }
 
 // Write a function that plays a single round of Rock Paper Scissors. The function should take two parameters - 
 // the playerSelection and computerSelection 
 
 function playRound(playerSelection, computerSelection) {
-
     // Make your function’s playerSelection parameter case-insensitive (so users can input rock, ROCK, RocK or any other variation).
     playerSelection = playerSelection.toLowerCase();
     playerSelection = playerSelection.replace(playerSelection.charAt(0),playerSelection.charAt(0).toUpperCase());
@@ -30,30 +24,22 @@ function playRound(playerSelection, computerSelection) {
     // "You lose! Paper beats Rock"
     // "You tie! Rock can't beat Rock"
     // "You win! Scissors lose to Rock"
-
     if (playerSelection === 'Rock' && computerSelection === 'Scissors' || playerSelection === "Paper" && computerSelection === "Rock" || 
     playerSelection === 'Scissors' && computerSelection === 'Paper') return `You win! ${computerSelection} lose to ${playerSelection}.`;
     else if (playerSelection === 'Rock' && computerSelection === 'Paper' || playerSelection === "Paper" && computerSelection === "Scissors" || 
     playerSelection === 'Scissors' && computerSelection === 'Rock') return `You lose! ${computerSelection} beats ${playerSelection}.`;
     else return `It's a tie. ${computerSelection} can't beat ${playerSelection}.`;
-
 }
 
 console.log(game());
 
-
-
-// Write a NEW function called game(). Call the playRound function inside of this one to play a 5 round game that keeps score 
-// and reports a winner or loser at the end.
-// Remember loops? This is a great opportunity to use one to play those five rounds:
-
 // Create a function game()
 function game() {
-// Create a variable to keep score
+    // Create a variable to keep score
     let humanScore = 0;
     let computerScore = 0;
 
-// Create a loop, so that a 5 round game is played
+    // Create a loop, so that a 5 round game is played
     for(let i = 0; i<5; i++) {
         // Inside the loop call playRound()  so that a round is played
         // Use prompt() to get input from the user.
@@ -61,8 +47,6 @@ function game() {
         console.log(result);
         if(result.includes('You win')) humanScore += 1;
         else if (result.includes('You lose')) computerScore += 1;
-
-
     }
 
     // At the end of the loop, report a winner, maybe return or console.log
@@ -75,14 +59,3 @@ function game() {
     }
 
 }
-
-
-
-
-
-
-
-
-
-
-
